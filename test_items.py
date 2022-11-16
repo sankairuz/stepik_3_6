3a3803d7f2de3f77b408879435573dfe00d763e4
sankairuz <sankairuz@gmail.com>
2022-11-16 17:29:41 +0300

    Файлы для задания

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-def test_basket_button(driver):
-    driver.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    assert len(driver.find_elements(By.CLASS_NAME, "btn-add-to-basket")) != 0, "Кнопка отсутствует"
+def test_basket_button(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    assert len(browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")) != 0, "Кнопка отсутствует"
